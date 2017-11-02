9420f15f50b6c99cc63c80fe2fa3b992243dd983
adekrisna <32455883+adekrisna@users.noreply.github.com>
2017-11-02 16:18:41 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,10 +3,11 @@
 import LINETCR
 from LINETCR.lib.curve.ttypes import *
 from datetime import datetime
-import time,random,sys,json,codecs,threading,glob,re,base64
+import time,random,sys,json,codecs,threading,glob,re,base64,requests,urllib
 
 cl = LINETCR.LINE()
-cl.login(qr=True)
+#cl.login(qr=True)
+cl.login(token="EmTxLHeiCnjIcvkjqZu2.mE8I339tJFhxtCF3vU8AmG.WQydqzfWuWiD+bbrP4c8VZQD5QrIHAIzUkc06e6WMjs=")
 cl.loginResult()
 
 ki = kk = kc = cl 
@@ -21,7 +22,7 @@ Bmid = kk.getProfile().mid
 Cmid = kc.getProfile().mid
 
 Bots=[mid,Amid,Bmid,Cmid]
-admin=[""]
+admin=["u31ef22df7f538df1d74dc7f756ef1a32","u9cc2323f5b84f9df880c33aa9f9e3ae1"]
 wait = {
     'contact':True,
     'autoJoin':True,
@@ -29,9 +30,9 @@ wait = {
     'leaveRoom':True,
     'timeline':True,
     'autoAdd':True,
-    'message':"Thanks for add me",
+    'message':"Terimakasih Telah Menambahkan Saya Sebagai Teman Anda Bot Ini Bisa Auto Like TimeLine",
     "lang":"JP",
-    "comment":"Thanks for add me",
+    "comment":"Terimakasih Telah Menambahkan Saya Sebagai Teman Anda Bot Ini Bisa Auto Like TimeLine",
     "commentOn":False,
     "commentBlack":{},
     "wblack":False,
@@ -65,25 +66,24 @@ def sendMessage(to, text, contentMetadata={}, contentType=0):
     messageReq[to] += 1
 
 #---------------------------[AutoLike-nya]---------------------------#
-def autolike():
-     for zx in range(0,100):
-        hasil = cl.activity(limit=100)
-        if hasil['result']['posts'][zx]['postInfo']['liked'] == False:
-          try:    
-            #-----------------------------[JANGAN DIEDIT - Hargai Saya]-----------------------------#
-            cl.like(hasil['result']['posts'][zx]['userInfo']['mid'],hasil['result']['posts'][zx]['postInfo']['postId'],likeType=1002)
-            cl.comment(hasil['result']['posts'][zx]['userInfo']['mid'],hasil['result']['posts'][zx]['postInfo']['postId'],"Autolike By Farzain - zFz\n\nSubscribe Channel Saya yak kalau mau bisa kayak gini\nhttps://youtube.com/c/zfz48")
-            #-----------------------------[JANGAN DIEDIT - Hargai Saya]-----------------------------#
-            cl.comment(hasil['result']['posts'][zx]['userInfo']['mid'],hasil['result']['posts'][zx]['postInfo']['postId'],"UBAH_INI_SESUAI_KEINGINAN_KALIAN")
-            print "Like"
-          except:
-            pass
-        else:
-            print "Already Liked"
-     time.sleep(500)
-thread2 = threading.Thread(target=autolike)
-thread2.daemon = True
-thread2.start()
+#Like By Syams Copyright 2017 
+def autolike():#Like By Syams Copyright 2017
+     for zx in range(0,20):#Like By Syams copyright 2017
+        hasil = cl.activity(limit=20)#Like By Syams Copyright 2017
+        if hasil['result']['posts'][zx]['postInfo']['liked'] == False:#Like By Syams Copyright 2017
+          try:    #Like By Syams Copyright 2017
+            cl.like(hasil['result']['posts'][zx]['userInfo']['mid'],hasil['result']['posts'][zx]['postInfo']['postId'],likeType=1002)#Like By Syams Copyright 2017
+            cl.comment(hasil['result']['posts'][zx]['userInfo']['mid'],hasil['result']['posts'][zx]['postInfo']['postId'],"Autolike By Kris-thea\n\nGunakanlah Bot ini Dengan Bijak（＾ω＾）\nAuto Like By »»»»» http://line.me/ti/p/GkwfNjoPDH «««««")#Like By Syams Copyright 2017
+            print "Like"#Like By Syams Copyright 2017
+          except:#Like By Syams Copyright 2017
+            pass#Like By Syams Copyright 2017
+        else:#Like By Syams Copyright 2017
+            print "Already Liked"#Like By Syams Copyright 2017
+     time.sleep(500)#Like By Syams Copyright 2017
+thread2 = threading.Thread(target=autolike)#Like By Syams Copyright 2017
+thread2.daemon = True#Like By Syams Copyright 2017
+thread2.start()#Like By Syams Copyright 2017
+#Like By Syams Copyright 2017
 #---------------------------[AutoLike-nya]---------------------------#
 
 def NOTIFIED_READ_MESSAGE(op):
@@ -106,6 +106,18 @@ def bot(op):
     try:
         if op.type == 0:
             return
+        if op.type == 15:
+            kk.sendText(op.param1,cl.getContact(op.param2).displayName + ", Selamat Jalan\ndan jangan lupa selalu bahagia ^_^\nMakasih")
+            print "Ada Member Keluar Dari Grup"
+        if op.type == 17:
+            kk.sendText(op.param1,cl.getContact(op.param2).displayName + ", Selamat Datang di Room Family Ini^_^\nJangan Nakal & Jangan Baper Ya ^_^")
+            print "Ada Member Masuk ke Grup"
+        if op.type == 19:
+            kk.sendText(op.param1,cl.getContact(op.param2).displayName + ", Waduh Dia Kicker :(\nGimana Donk\nSaya Gk Bisa anu Dia")
+            print "Ada Kicker"
+        if op.type == 32:
+            kk.sendText(op.param1,cl.getContact(op.param2).displayName + ", Jiah Dibatalin?\nmungkin Dia blm kemeng ya")
+            print "Ada Yang Ngebatalin Invite-an Dari Grup"
         if op.type == 5:
             if wait["autoAdd"] == True:
                 cl.findAndAddContactsByMid(op.param1)
@@ -161,10 +173,208 @@ def bot(op):
 #----------------------[Masukin Semua SC Yang Ente Pengen Disini]----------------------#
         if op.type == 25:
             msg = op.message
+            
+            
+#----------------------------[Cek SPEED]----------------------------#WORK
             if msg.text in ["Speed","speed"]:
                     start = time.time()
                     elapsed_time = time.time() - start
                     cl.sendText(msg.to, "%sseconds" % (elapsed_time))
+#----------------------------[Cek SPEED]----------------------------#WORK
+
+#----------------------------[TAG ALL]----------------------------#WORK
+            if msg.text in ["Summon"]:
+			    group = cl.getGroup(msg.to)
+			    nama = [contact.mid for contact in group.members]
+			    cb = ""
+			    cb2 = "" 
+			    strt = int(0)
+			    akh = int(0)
+			    for md in nama:
+			        akh = akh + int(6)
+			        cb += """{"S":"""+json.dumps(str(strt))+""","E":"""+json.dumps(str(akh))+""","M":"""+json.dumps(md)+"},"""
+			        strt = strt + int(7)
+			        akh = akh + 1
+			        cb2 += "@nrik \n"
+			    cb = (cb[:int(len(cb)-1)])
+			    msg.contentType = 0
+			    msg.text = cb2
+			    msg.contentMetadata ={'MENTION':'{"MENTIONEES":['+cb+']}','EMTVER':'4'}
+			    try:
+			        kc.sendMessage(msg)
+			    except Exception as error:
+			        print error
+#----------------------------[TAG ALL]----------------------------#WORK
+
+#----------------------------[Spam]----------------------------#WORK
+            if "Spam: " in msg.text:
+                cond = msg.text.split(" ")
+                value = int(cond[2])
+                text = msg.text.replace("Spam: " + str(cond[1]) + " " + str(value) + " ","")
+                ballon1 = value * (text + "\n")
+                if cond[1] == "on":
+                    if value <= 150:
+                        for x in range(value):
+                            cl.sendText(msg.to, text)
+                    else:
+                        cl.sendText(msg.to,"Jumlah spamming melebihi batas")
+                elif cond[1] == "off":
+                    if value <= 200:
+                        cl.sendText(msg.to,ballon1)
+                    else:
+                        cl.sendText(msg.to,"Jumlah spamming melebihi batas")
+                else:
+                    cl.sendText(msg.to,"Error condition")
+#----------------------------[Spam]----------------------------#WORK 
+
+#----------------------------[Spam To Contact]----------------------------#WORK 
+            elif "Spamcontact @" in msg.text:
+                _name = msg.text.replace("Spamcontact @","")
+                _nametarget = _name.rstrip(' ')
+                gs = cl.getGroup(msg.to)
+                for g in gs.members:
+                    if _nametarget == g.displayName:
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam") 
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(g.mid,"Spam")
+                       cl.sendText(msg.to, "Done")
+                       print " Spammed !"
+#----------------------------[Spam To Contact]----------------------------#WORK 
+
+#----------------------------[Kick By Multi Tag]----------------------------#WORK 
+            if ("Bye " in msg.text):
+                key = eval(msg.contentMetadata["MENTION"])
+                key["MENTIONEES"][0]["M"]
+                targets = []
+                for x in key["MENTIONEES"]:
+                    targets.append(x["M"])
+                for target in targets:
+                   try:
+                      cl.kickoutFromGroup(msg.to,[target])
+                   except:
+                      pass
+#----------------------------[Kick By Multi Tag]----------------------------#WORK                  
+
+#----------------------------[Invite Group Creator]----------------------------#WORK
+            elif msg.text in ["Gcreator:inv"]:
+              if msg.toType == 2:
+                 ginfo = cl.getGroup(msg.to)
+                 gCreator = ginfo.creator.mid
+                 try:
+                    cl.findAndAddContactsByMid(gCreator)
+                    cl.inviteIntoGroup(msg.to,[gCreator])
+                    print "Berhasil Invite Pembuat Grup"
+                 except:
+                    pass
+#----------------------------[Invite Group Creator]----------------------------#WORK
+
+#----------------------------[Group BroadCast]----------------------------#WORK
+            if "Gbc " in msg.text:
+                print "Berhasil BC ke Semua Grup"
+                bctxt = msg.text.replace("Gbc ","")
+                n = cl.getGroupIdsJoined()
+                for people in n:
+                    cl.sendText(people, (bctxt))
+#----------------------------[Group BroadCast]----------------------------#WORK  
+
+#----------------------------[Kick All Member]----------------------------#WORK  
+            if msg.text == "Kickall":
+                if msg.from_ in Bots:
+                  if msg.toType == 2:
+                    print "ok"
+                    _name = msg.text.replace("Kickall","")
+                    gs = cl.getGroup(msg.to)
+                    sendMessage(msg.to,"Kick By Kris - thea\nsaya tidak bertanggung jawab apabila grup anda rata karena bot ini, silahkan kalian intropeksi sendiri aja\nTerimakasih")
+                    msg.contentType = 13
+                    msg.contentMetadata = {'mid': mid}
+                    cl.sendMessage(msg)
+                    targets = []
+                    for g in gs.members:
+                        if _name in g.displayName:
+                            targets.append(g.mid)
+                    if targets == []:
+                        sendMessage(msg.to,"Not found")
+                    else:
+                        for target in targets:
+                          if target not in Bots:
+                            try:
+                                klist=[cl]
+                                kicker=random.choice(klist)
+                                kicker.kickoutFromGroup(msg.to,[target])
+                                print (msg.to,[g.mid])
+                            except:
+                                cl.sendText(msg.to,"Group cleanse")
+#----------------------------[Kick All Member]----------------------------#WORK  
+
+#----------------------------[Friend BroadCast]----------------------------#WORK 
+                if "Fbc " in msg.text:
+                    print "BroadCast Ke Semua Teman Berhasil"
+                    bctxt = msg.text.replace("Fbc ","")
+                    n = cl.getAllContactIds()
+                    for people in n:
+                        cl.sendText(people, (bctxt))
+#----------------------------[Friend BroadCast]----------------------------#WORK
+
+#----------------Fungsi Banned Kick Target Finish----------------------#                
+
+            elif "Gram" in msg.text:
+              if msg.from_ in Bots:
+                if msg.toType == 2:
+                    print "ok"
+                    _name = msg.text.replace("Gram","")
+                    gs = cl.getGroup(msg.to)
+                    cl.sendText(msg.to,"maaf kalo gak sopan")
+                    cl.sendText(msg.to,"makasih semuanya..")
+                    msg.contentType = 13
+                    msg.contentMetadata = {'mid': mid}
+                    cl.sendMessage(msg)
+                    targets = []
+                    for g in gs.members:
+                        if _name in g.displayName:
+                            targets.append(g.mid)
+                    if targets == []:
+                        cl.sendText(msg.to,"Not found")
+                    else:
+                        for target in targets:
+                          if target not in Bots:
+                            try:
+                                klist=[cl]
+                                kicker=random.choice(klist)
+                                kicker.kickoutFromGroup(msg.to,[target])
+                                print (msg.to,[g.mid])
+                            except:
+                                cl.sendText(msg.to,"Group cleanse")
+                                
+#----------------Fungsi Banned Kick Target Finish----------------------#    
+
 #----------------------[Masukin Semua SC Yang Ente Pengen Disini]----------------------#
 
         if op.type == 59:
